49c9d5c05e1d8d716a0e5456b5e11354074e208b
Stef McCuistion <stef.mccuistion@gmail.com>
2026-08-15 01:27:15 -0500

    Hide dialogue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,32 @@
-// vars
-let click_count = 0
-
 // game instance
 function Game() {
-  this.character_names = ['ExampleName1', 'ExampleName2'],
+  this.character_names = ['ExampleName1', 'ExampleName2'];
+
   this.run = function() {
-    console.log("Hi! My name is " + this.character_names[0] + ".");
-    console.log("Here's a second page. ");
+    this.index_dialogue();
+    this.hide_dialogue();
+    this.addClickListener();
+  }
+
+  this.index_dialogue = function() {
+    this.dialogue_list = document.querySelectorAll(".dialogue");
+    console.log("pages of dialogue = " + this.dialogue_list.length);
+  }
+
+  this.hide_dialogue = function() {
+    for (let i = 1; i <= this.dialogue_list.length; i++) {
+      this.dialogue_list[i-1].style.display = "none";
+    }
+  }
+
+  this.addClickListener = function() {
+    let current_page_num = 1;
+    document.addEventListener('click', function(event) {
+      current_page_num ++;
+      console.log('go to page ' + current_page_num);
+    }, {capture: true});
   }
 };
 
 const game_instance = new Game();
 game_instance.run()
-
-// click counter
-document.addEventListener('click', function(event) {
-    click_count ++
-    console.log('YOU CLICKED' + click_count);
-}, { capture: true });
