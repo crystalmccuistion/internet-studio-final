82d9de21a55c8ee93095c962645a548ec496f06a
Stef McCuistion <stef.mccuistion@gmail.com>
2026-08-15 00:41:23 -0500

    Add click counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-window.addEventListener('click', function(event) {
-    console.log('YOU CLICKED.');
-}, { capture: true });
+// vars
+let click_count = 0
 
+// game instance
 function Game() {
   this.character_names = ['ExampleName1', 'ExampleName2'],
   this.run = function() {
@@ -12,3 +12,9 @@ function Game() {
 
 const game_instance = new Game();
 game_instance.run()
+
+// click counter
+document.addEventListener('click', function(event) {
+    click_count ++
+    console.log('YOU CLICKED' + click_count);
+}, { capture: true });
